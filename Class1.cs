6a9d72ba13d95d144c6316f41829a5dd0279ae5e
Erik JS <erikjs_br@hotmail.com>
2020-06-03 14:46:59 -0300

    First commit

---
+++ Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopIconsApp
+{
+    class Class1
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetShellWindow();
+
+
+        public static IntPtr GetSysListView32()
+        {
+            IntPtr handleProgman, handleShelldll, handleSysListView32;
+            handleProgman = FindWindow("Progman", null);
+            handleShelldll = FindWindowEx(handleProgman, IntPtr.Zero, "SHELLDLL_DefView", null);
+            if (handleShelldll == IntPtr.Zero)
+            {
+                EnumWindows(delegate (IntPtr hWnd, IntPtr lParam) {
+                    if (GetClassNameString(hWnd) == "WorkerW")
+                    {
+                        handleShelldll = FindWindowEx(hWnd, IntPtr.Zero, "SHELLDLL_DefView", null);
+                        if (handleShelldll != IntPtr.Zero)
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                , IntPtr.Zero);
+            }
+            handleSysListView32 = FindWindowEx(handleShelldll, IntPtr.Zero, "SysListView32", null);
+            return handleSysListView32;
+        }
+
+        public static int GetDesktopItemCount(IntPtr desktopListViewHandle)
+        {
+            IntPtr iconccount = SendMessage(desktopListViewHandle, MessageConst.LVM_GETITEMCOUNT, 0, 0);
+            return iconccount.ToInt32();
+        }
+
+        public static string GetClassNameString(IntPtr hWnd)
+        {
+            StringBuilder sb = new StringBuilder(128);
+            int a = GetClassName(hWnd, sb, 128);
+            if (a == 0) return "";
+            return sb.ToString();
+        }
+
+        public static void LogText(string text)
+        {
+            Form1.mainForm.Invoke((MethodInvoker)delegate
+            {
+                Form1.listbox.Items.Add(text);
+            });
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopIconsApp
+{
+    public partial class Form1 : Form
+    {
+
+        public static Form1 mainForm;
+
+        public static ListBox listbox;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mainForm = this;
+            listbox = listBox1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr handle = Class1.GetSysListView32();
+            listBox1.Items.Add(handle.ToInt32().ToString("X8"));
+            int count = Class1.GetDesktopItemCount(handle);
+            listBox1.Items.Add(count.ToString());
+        }
+
+    }
+}
